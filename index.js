8c1d527bf427e02359655cf93630bf16ca5fd634
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-15 05:27:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function placeOrder(cardNumber) {
    return "Sorry, we don't have a credit card on file for you."
   }
   cart = 0
-  return `Your total cost is ${total().total}, which will be charged to the card ${cardNumber}.`
+  var total1=total()
+  return `Your total cost is ${total1}, which will be charged to the card ${cardNumber}.`
   
 }
